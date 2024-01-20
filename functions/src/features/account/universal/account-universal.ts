378126df21495477903d671b4faee69bec60de34
Jason Rich Darmawan <63768126+jasonrichdarmawan@users.noreply.github.com>
2024-01-20 23:12:40 +0700

    chore: trigger ci/cd

--- functions/src/features/account/universal/account-universal.ts
+++ functions/src/features/account/universal/account-universal.ts
@@ -3,6 +3,7 @@ import {info} from "firebase-functions/logger";
 
 export const accountUniversal = onRequest((request, response) => {
   info("accountUniversal");
+
   // eslint-disable-next-line @typescript-eslint/no-var-requires
   require("../../../../dist/account/server/main")
     .app()(request, response);
--- functions/src/features/experiences/universal/experiences-universal.ts
+++ functions/src/features/experiences/universal/experiences-universal.ts
@@ -3,7 +3,6 @@ import {info} from "firebase-functions/logger";
 
 export const experiencesUniversal = onRequest((request, response) => {
   info("experiencesUniversal");
-
   // eslint-disable-next-line @typescript-eslint/no-var-requires
   require("../../../../dist/experiences/server/main")
     .app()(request, response);
