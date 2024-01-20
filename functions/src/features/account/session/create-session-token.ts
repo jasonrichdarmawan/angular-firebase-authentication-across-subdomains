fc9c513707f6ea590292cdc2de3fba53f0efb1c3
Jason Rich Darmawan <63768126+jasonrichdarmawan@users.noreply.github.com>
2024-01-20 22:26:20 +0700

    fix: CI/CD

--- functions/src/features/account/session/create-session-token.ts
+++ functions/src/features/account/session/create-session-token.ts
@@ -55,7 +55,6 @@ const handleCors = (req: Request, res: Response) => {
   const allowedOrigins = ["https://account.topoint.org", "https://experiences.topoint.org", "https://checkout.topoint.org"];
   const origin = req.get("origin") ?? "";
 
-
   if (allowedOrigins.includes(origin)) {
     res.set("Access-Control-Allow-Origin", origin);
   }
