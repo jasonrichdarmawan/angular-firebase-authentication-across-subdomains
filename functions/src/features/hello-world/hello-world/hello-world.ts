085c2adbfce2a124dc0749ac241b53101ec59d08
Jason Rich Darmawan <63768126+jasonrichdarmawan@users.noreply.github.com>
2024-01-20 12:19:28 +0700

    fix: CI/CD

--- functions/src/features/hello-world/hello-world/hello-world.ts
+++ functions/src/features/hello-world/hello-world/hello-world.ts
@@ -1,14 +1,17 @@
 // reference: https://firebase.google.com/docs/functions/http-events?gen=2nd
 
 import {onRequest} from "firebase-functions/v2/https";
-import {Request, Response} from "express";
 
-export const helloWorld = onRequest((req, res) => {
+import express = require("express");
+const app = express();
+
+app.post("/", (req, res) => {
   handleCors(req, res);
+
   res.send({data: "Hello World"});
 });
 
-const handleCors = (req: Request, res: Response) => {
+const handleCors = (req: express.Request, res: express.Response) => {
   const allowedOrigins = ["https://account.topoint.org", "https://experiences.topoint.org", "https://checkout.topoint.org"];
   const origin = req.get("origin") ?? "";
 
@@ -20,3 +23,5 @@ const handleCors = (req: Request, res: Response) => {
     res.set("Access-Control-Allow-Origin", "*");
   }
 };
+
+export const helloWorld = onRequest(app);
