86c0104e3d0a472159e9d671b83e72039230747b
Jason Rich Darmawan <63768126+jasonrichdarmawan@users.noreply.github.com>
2024-01-20 12:29:28 +0700

    fix: hello World

--- functions/src/features/hello-world/hello-world/hello-world.ts
+++ functions/src/features/hello-world/hello-world/hello-world.ts
@@ -19,4 +19,13 @@ const handleCors = (req: Request, res: Response) => {
   if (process.env.FUNCTIONS_EMULATOR) {
     res.set("Access-Control-Allow-Origin", "*");
   }
+
+  if (req.method === "OPTIONS") {
+    // Send response to OPTIONS requests
+    res.set("Access-Control-Allow-Methods", "GET");
+    res.set("Access-Control-Allow-Headers", "Content-Type");
+    res.set("Access-Control-Max-Age", "3600");
+    res.status(204).send("");
+    return;
+  }
 };
